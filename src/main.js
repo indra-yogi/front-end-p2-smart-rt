ec488b35ed98bdb1dc887c2cea269ff83ddbec65
Yogi <indrayp18@gmail.com>
2021-08-12 02:17:07 +0800

    Remove vue select

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
 import Vue from "vue";
 import App from "@/App.vue";
 import Vuesax from "vuesax"
-import vSelect from 'vue-select'
 
 import 'vuesax/dist/vuesax.css'; //Vuesax styles
-import 'vue-select/dist/vue-select.css';
 import 'noty/lib/themes/mint.css'
 import 'noty/lib/noty.css'
 import 'material-icons/iconfont/material-icons.css';
@@ -19,7 +17,6 @@ Vue.use(
   //option here
 )
 Vue.component('apexchart', VueApexCharts);
-Vue.component('v-select', vSelect)
 
 Vue.config.productionTip = false
 new Vue({
